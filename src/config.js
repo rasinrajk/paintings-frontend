de72f325874e5b1222d5c9d620160d16eeda5bf7
rasin.raj <rasin.raj@netobjex.com>
2020-12-16 01:18:09 +0300

    setting up react App

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "paintingsbucket",
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://z6m4kc8fs4.execute-api.us-east-2.amazonaws.com/dev",
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_8VNgKvAPh",
+    APP_CLIENT_ID: "5rdv8jhmgmivodiach0g39np1o",
+    IDENTITY_POOL_ID: "us-east-2:e963538c-6244-4e03-b46b-6046a1851d1c",
+  },
+};
+
+export default config;
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Kanvas</h1>
+        <p className="text-muted">A paintings gallery app</p>
+      </div>
+    </div>
+  );
+}
